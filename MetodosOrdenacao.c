2340b47ce217d990a23cbbd22c69d2d708c3b09c
Matheus Machado <matheuswaldivino1@gmail.com>
2024-10-31 15:38:59 -0300

    Alterações 31/10

--- MetodosOrdenacao.c
+++ MetodosOrdenacao.c
@@ -3,37 +3,38 @@
 #include <time.h>
 #include <locale.h>
 #define TAMARR 50000
-#define TAMARR1 TAMARR-100
+#define TAMARR1 TAMARR-200
 
-void imprimeMenu (){
+void imprimirMenu(){
     printf("+-------------------------------------------------------------+\n");
-    printf("|(01) Preencher o array com números aleatórios                |\n");
-    printf("|(02) Ordenar o array pelo método Bubble Sort                 |\n");
-    printf("|(03) Ordenar o array pelo método Insertion Sort              |\n");
-    printf("|(04) Ordenar o array pelo método Selection Sort              |\n");
-    printf("|(05) Ordenar o array pelo método Heap Sort                   |\n");
-    printf("|(06) Ordenar o array pelo método Quick Sort                  |\n");
-    printf("|(07) Ordenar o array pelo método Merge Sort                  |\n");
-    printf("|(08) Ordenar o array pelo método Shell Sort                  |\n");
-    printf("|(09) Ordenar o array pelo método ...                         |\n");
-    printf("|(10) Ordenar o array pelo método ...                         |\n");
-    printf("|(11) Ordenar o array pelo método ...                         |\n");
+    printf("|(1) Preencher o array com números aleatórios                 |\n");
+    printf("|(2) Ordenar o array pelo método Bubble Sort                  |\n");
+    printf("|(3) Ordenar o array pelo método Selection Sort               |\n");
+    printf("|(4) Ordenar o array pelo método Insertion Sort               |\n");
+    printf("|(5) Ordenar o array pelo método Heap Sort                    |\n");
+    printf("|(6) Ordenar o array pelo método Quick Sort                   |\n");
+    printf("|(7) Ordenar o array pelo método Merge Sort                   |\n");
+    printf("|(8) Ordenar o array pelo método Shell Sort                   |\n");
+    printf("|(9) Ordenar o array pelo método Bucket Sort                  |\n");
+    printf("|(10) Ordenar o array pelo método Counting Sort               |\n");
+    printf("|(11) Ordenar o array pelo método Radix Sort                  |\n");
     printf("|(12) Imprimir resultados dos métodos de ordenação executados |\n");
     printf("|(13) Imprimir o array                                        |\n");
-    printf("|(00) Encerrar o programa                                     |\n");
+    printf("|(0) Encerrar o programa                                      |\n");
     printf("+-------------------------------------------------------------+\n");
+    printf("Digite sua opção: ");
 }
 
-void preencheArray (int arr[]){
+void preencherArray(int arr[]){
     int i;
     for(i=0; i<TAMARR; i++){
         arr[i] = rand()%200000;
     }
 }
 
-void imprimeArray (int arr[]){
+void imprimirArray(int arr[]){
     int i;
-    for(i=0; i<100; i++){
+    for(i=0; i<200; i++){
         printf("%7d", arr[i]);
     }
     printf("    ...");
@@ -42,8 +43,23 @@ void imprimeArray (int arr[]){
     }
 }
 
-void bubbleSort(int arr[]){
+double mostrarResultado(int arr[], double (*funcao)(int)){
+    double sortTime;
+
+    imprimirArray(arr);
+    sortTime = funcao(arr);
+    printf("\n\n");
+    imprimirArray(arr);
+    printf("\n\nTempo de execução da ordenação: %.3f segundos\n\n", sortTime);
+    sleep(10);
+    system("cls");
+    return sortTime;
+}
+
+double bubbleSort(int arr[]){
     int i,j,trocaNum;
+    double sortTime;
+    clock_t start = clock();
 
     for(i=0; i<TAMARR; i++){
 
@@ -57,11 +73,17 @@ void bubbleSort(int arr[]){
             }
         }
     }
-    printf("\nArray Ordenado\n");
+
+    clock_t end = clock();
+    double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    sortTime = cpu_time_used;
+    return sortTime;
 }
 
-void selectionSort(int arr[]){
+double selectionSort(int arr[]){
     int i,j, min;
+    double sortTime;
+    clock_t start = clock();
 
     for(i=0; i<TAMARR - 1; i++) {
     	min = 0;
@@ -74,34 +96,90 @@ void selectionSort(int arr[]){
 		arr[min] = arr[i];
 		arr[i] = temp;
 	}
-	printf("\nArray Ordenado\n");
+	clock_t end = clock();
+    double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    sortTime = cpu_time_used;
+    return sortTime;
 }
 
+//void mostrarTodosResultados(double bubble, double selection, double insertion, double heap, double quick, double merge, double shell, double bucket, double counting, double radix)
+
 int main(){
+    int arr[TAMARR];
+    int opMenu;
+    double bubbleTime = 0, selectionTime = 0, insertionTime = 0, heapTime = 0, quickTime = 0, mergeTime = 0, shellTime = 0, bucketTime = 0, countingTime = 0, radixTime = 0;
+
     setlocale(LC_ALL, "Portuguese");
 
-    imprimeMenu();
-
-    int arr[TAMARR];
-
     srand(time(NULL));
+    preencherArray(arr);
 
-    preencheArray(arr);
-    imprimeArray(arr);
+    do{
+        imprimirMenu();
+        scanf("%d", &opMenu);
 
-    // Captura o tempo antes da execução da função
-    clock_t start = clock();
-
-    selectionSort(arr);
-	imprimeArray(arr);
-
-    // Captura o tempo após a execução da função
-    clock_t end = clock();
-
-    double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC; // Calcula o tempo em segundos
-
-    printf("\nTempo de execução da ordenação: %f segundos\n", cpu_time_used);
-    printf("\n\n\n");
+        switch (opMenu){
+            case 1:
+                preencherArray(arr);
+                break;
+            case 2:
+                bubbleTime = mostrarResultado(arr, bubbleSort);
+                break;
+            case 3:
+                selectionTime = mostrarResultado(arr, selectionSort);
+                break;
+            /*
+            case 4:
+                insertionSort (arr);
+                break;
+            case 5:
+                heapSort (arr);
+                break;
+            case 6:
+                quickSort (arr);
+                break;
+            case 7:
+                mergeSort (arr);
+                break;
+            case 8:
+                shellSort (arr);
+                break;
+            case 9:
+                bucketSort (arr);
+                break;
+            case 10:
+                countingSort (arr);
+                break;
+            case 11:
+                radixSort (arr);
+                break;
+            */
+            case 12:
+                if (bubbleTime == 0){
+                    bubbleTime = bubbleSort(arr);
+                }
+                if (selectionTime == 0){
+                    selectionTime = selectionSort(arr);
+                }
+                printf("+-------------------------------------------------------------+\n");
+                printf("|                  Resultados das ordenações                  |\n");
+                printf("|Bubble   : %.3f segundos                                    |\n", bubbleTime);
+                printf("|Selection: %.3f segundos                                    |\n", selectionTime);
+                printf("|Insertion: %.3f segundos                                    |\n", insertionTime);
+                printf("|Heap     : %.3f segundos                                    |\n", heapTime);
+                printf("|Quick    : %.3f segundos                                    |\n", quickTime);
+                printf("|Merge    : %.3f segundos                                    |\n", mergeTime);
+                printf("|Shell    : %.3f segundos                                    |\n", shellTime);
+                printf("|Bucket   : %.3f segundos                                    |\n", bucketTime);
+                printf("|Counting : %.3f segundos                                    |\n", countingTime);
+                printf("|Radix    : %.3f segundos                                    |\n", radixTime);
+                printf("+-------------------------------------------------------------+\n");
+                break;
+            case 13:
+                imprimirArray(arr);
+                break;
+        }
+    }while (opMenu != 0);
 
     return 0;
 }
